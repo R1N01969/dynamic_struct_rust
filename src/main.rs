74943c6d1a83febcc57d7682ef2cf74f673e8473
R1N0\hgms1 <hagamasa1@gmail.com>
2022-10-02 22:57:25 +0900

    サンプルのWebAPIを呼び出すように変更

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Client;
+use serde::Deserialize;
+
+mod lib;
+
+use crate::lib::get_key_from_json;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let client = Client::new();
+    let url = "https://zipcloud.ibsnet.co.jp/api/search";
+    let response = client
+        .get(url)
+        .query(&[("zipcode", "1000002")])
+        .send()
+        .await?;
+    let body = response.text().await?;
+    let keys: Vec<String> = get_key_from_json(body);
+    for key in keys {
+        println!("{}", key);
+    }
+    Ok(())
 }
